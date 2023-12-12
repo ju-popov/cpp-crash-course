1bce5618fa9446527800f125ac6932b534df5e9d
Julian Popov <julian.popov@gmail.com>
2023-12-12 15:25:30 +0200

    Update ch4.cpp

--- ch4.cpp
+++ ch4.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sys/time.h>
-#include <unistd.h>
-
-using namespace std;
+#include <chrono>
+#include <thread>
 
 struct TimerClass {
-    TimerClass(const char* name) : name{name} {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-
-        long long timestamp = tv.tv_sec * 1000LL + tv.tv_usec / 1000;
-
-        this->timestamp = timestamp;
-
-        cout << this->name << ": TimerClass() timestamp: " << timestamp << endl;
+    TimerClass(const char* name) : name{name}, timestamp{getCurrentTimestamp()} {
+        std::cout << this->name << ": TimerClass() timestamp: " << timestamp << std::endl;
     }
 
     ~TimerClass() {
@@ -23,24 +14,16 @@ struct TimerClass {
             return;
         }
 
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
+        long long age = getCurrentTimestamp() - this->timestamp;
 
-        long long timestamp = tv.tv_sec * 1000LL + tv.tv_usec / 1000;
-        long long age = timestamp - this->timestamp;
-
-        cout << this->name << ": ~TimerClass() timestamp: " << timestamp << " age: " << age << endl;
+        std::cout << this->name << ": ~TimerClass() timestamp: " << this->timestamp << " age: " << age << std::endl;
     }
 
-    TimerClass(const TimerClass& other) {
-        this->name = other.name;
-        this->timestamp = other.timestamp;
-    }
+    TimerClass(const TimerClass& other) : name{other.name}, timestamp{other.timestamp} {}
 
     TimerClass& operator=(const TimerClass& other) {
         if (this == &other) {
             return *this;
-
         }
 
         this->name = other.name;
@@ -49,15 +32,12 @@ struct TimerClass {
         return *this;
     }
 
-    TimerClass(TimerClass&& other) noexcept{
-        this->name = other.name;
-        this->timestamp = other.timestamp;
-
+    TimerClass(TimerClass&& other) noexcept : name{other.name}, timestamp{other.timestamp} {
         other.timestamp = 0;
         other.name = nullptr;
     }
 
-    TimerClass& operator=(TimerClass&& other) noexcept{
+    TimerClass& operator=(TimerClass&& other) noexcept {
         if (this == &other) {
             return *this;
         }
@@ -65,15 +45,21 @@ struct TimerClass {
         this->name = other.name;
         this->timestamp = other.timestamp;
 
-        other.timestamp = 0;
         other.name = nullptr;
+        other.timestamp = 0;
 
         return *this;
     }
 
-    private:
-        const char* name;
-        long long timestamp;
+private:
+    const char* name;
+    long long timestamp;
+
+    long long getCurrentTimestamp() const {
+        auto now = std::chrono::system_clock::now();
+        auto duration = now.time_since_epoch();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    }
 };
 
 int main()
@@ -83,11 +69,10 @@ int main()
     TimerClass timerOneAssignmentOperator = timerOne;
 
     TimerClass timerTwo{"two"};
-    TimerClass timerTwoMoveConstructor{move(timerTwo)};
+    TimerClass timerTwoMoveConstructor{std::move(timerTwo)};
 
     TimerClass timerThree{"three"};
-    TimerClass timerThreeMoveAssignmentOperator = move(timerThree);
+    TimerClass timerThreeMoveAssignmentOperator = std::move(timerThree);
 
-    usleep(10000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
-
