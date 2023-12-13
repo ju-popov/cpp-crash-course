6e8ab08228b3e833b35dd81cc1ab096719632f48
Julian Popov <julian.popov@gmail.com>
2023-12-13 18:09:43 +0200

    Update ch5.cpp

--- ch5.cpp
+++ ch5.cpp
@@ -1,19 +1,43 @@
 #include <unordered_map>
 #include <iostream>
 
+class Logger {
+public:
+    Logger() = default;
+    virtual ~Logger() = default;
+
+    virtual void transfer(const long fromAccount, const long toAccount, const long long amount) const = 0;
+};
+
+class ConsoleLogger : public Logger {
+public:
+    explicit ConsoleLogger(const char* name) : name{name} {}
+
+    void transfer(const long fromAccount, const long toAccount, const long long amount) const override {
+        std::cout << this->name << ": " << 
+            "transfer from account: " << fromAccount <<
+            " to account: " << toAccount <<
+            " amount: " << amount <<
+            std::endl;
+    }
+
+private:
+    const char* name;
+};
+
 class AccountDatabase {
 public:
     AccountDatabase() = default;
     virtual ~AccountDatabase() = default;
 
-    virtual long long getAmount(const long accountId) const = 0;
-    virtual void setAmount(const long accountId, const long long amount) = 0;
+    virtual long long getAmount(const long account) const = 0;
+    virtual void setAmount(const long account, const long long amount) = 0;
 };
 
 class InMemoryAccountDatabase : public AccountDatabase {
 public:
-    long long getAmount(const long accountId) const override {
-        auto it = this->accounts.find(accountId);
+    long long getAmount(const long account) const override {
+        auto it = this->accounts.find(account);
         if (it == this->accounts.end()) {
             return 0;
         }
@@ -21,36 +45,12 @@ public:
         return it->second;
     }
 
-    void setAmount(const long accountId, const long long amount) override {
-        this->accounts[accountId] = amount;
+    void setAmount(const long account, const long long amount) override {
+        this->accounts[account] = amount;
     }
 
 private:
     std::unordered_map<long, long long> accounts;
-};
-
-class Logger {
-public:
-    Logger() = default;
-    virtual ~Logger() = default;
-
-    virtual void transfer(const long fromAccountId, const long toAccountId, const long long amount) const = 0;
-};
-
-class ConsoleLogger : public Logger {
-public:
-    explicit ConsoleLogger(const char* name) : name{name} {}
-
-    void transfer(const long fromAccountId, const long toAccountId, const long long amount) const override {
-        std::cout << this->name << ": " << 
-            "transfer from account: " << fromAccountId <<
-            " to account: " << toAccountId <<
-            " amount: " << amount <<
-            std::endl;
-    }
-
-private:
-    const char* name;
 };
 
 class Bank {
@@ -61,16 +61,16 @@ public:
         this->logger = logger;
     }
 
-    void transfer(const long fromAccountId, const long toAccountId, const long long amount) {
+    void transfer(const long fromAccount, const long toAccount, const long long amount) {
         if (this->logger != nullptr) {
-            this->logger->transfer(fromAccountId, toAccountId, amount);
+            this->logger->transfer(fromAccount, toAccount, amount);
         }
 
-        auto fromAccountAmount = this->accountDatabase.getAmount(fromAccountId);
-        auto toAccountAmount = this->accountDatabase.getAmount(toAccountId);
+        auto fromAccountAmount = this->accountDatabase.getAmount(fromAccount);
+        auto toAccountAmount = this->accountDatabase.getAmount(toAccount);
 
-        this->accountDatabase.setAmount(fromAccountId, fromAccountAmount - amount);
-        this->accountDatabase.setAmount(toAccountId, toAccountAmount + amount);
+        this->accountDatabase.setAmount(fromAccount, fromAccountAmount - amount);
+        this->accountDatabase.setAmount(toAccount, toAccountAmount + amount);
     }
 
 private:
